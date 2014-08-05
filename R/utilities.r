b5ca6654d7d662cba08706800c14b8518e0eefa5
assaforon <assaf.oron@gmail.com>
2014-08-04 18:38:39 -0700

    Back to analytic CIs....
    ....coded the 3 'recommended' ones per Brown et al. 2001.

--- R/utilities.r
+++ R/utilities.r
@@ -32,3 +32,32 @@ if(!full) return(endpoints)
 return(list(endpoints=endpoints,pivots=pivots))
 }
 
+#### Some standard CI calculation functions
+
+wilsonCI<-function(phat,n,conf=0.9)
+{
+zalpha=qnorm(1-(1-conf)/2)
+wid=zalpha*sqrt((phat*(1-phat)+zalpha^2/(4*n))
+/n)
+return(c(max(0,(phat+zalpha^2/(2*n)-wid)/(1+zalpha^2/n)),
+	min(1,(phat+zalpha^2/(2*n)+wid)/(1+zalpha^2/n))))
+}
+
+agcouCI<-function(phat,n,conf=0.9)
+{
+zalpha=qnorm(1-(1-conf)/2)
+ptilde=(phat+zalpha^2/(2*n))/(1+zalpha^2/n)
+ntilde=n+zalpha^2
+wid=zalpha*sqrt(ptilde*(1-ptilde)/ntilde)
+return(c(max(0,ptilde-wid),
+	min(1,ptilde+wid)))
+}
+
+jeffCI<-function(phat,n,conf=0.9,w1=0.5,w2=w1)
+{
+x=n*phat
+clow=ifelse(phat==0,0,qbeta((1-conf)/2,x+w1,n-x+w2))
+chigh=ifelse(phat==1,1,qbeta(1-(1-conf)/2,x+w1,n-x+w2))
+
+return(c(clow,chigh))
+}
