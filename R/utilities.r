19390951ac3fa5d3eaf009e3febcec4a3e69ce90
assaforon <assaf.oron@gmail.com>
2014-08-29 17:47:22 -0700

    modified inverse CI
    global extrapolation rulz!!!!
    Also, started to remove bootstrap functions.

--- R/utilities.r
+++ R/utilities.r
@@ -1,37 +1,3 @@
-
-# one-sixth the skew of the Binomial score
-scoreSkew6<-function(theta,n) (1-2*theta)/(6*sqrt(n*theta*(1-theta)))
-
-### Non-vectorized for now
-bcaFwd<-function(bootdat,thetaHat,n,probs,full=FALSE)
-{
-# BCa parameters
-z0=qnorm(mean(bootdat<=thetaHat))
-accel=scoreSkew6(thetaHat,n)
-cat(c(z0,accel),'\n')
-
-zalpha=qnorm(probs)
-zbca=z0+(z0+zalpha)/(1-accel*(z0+zalpha))
-cat(zbca,'\n')
-endpoints=quantile(bootdat,prob=pnorm(zbca),type=6)
-if(!full) return(endpoints)
-return(list(enddpoints=endpoints,a=accel,z0=z0))
-}
-
-
-#### bootstrap-t for Binomial, with variance shrinkage
-bootTFwd<-function(bootdat,thetaHat,n,probs,full=FALSE,prior,nprior)
-{
-theta0Shrink=(thetaHat*n+prior*nprior)/(n+nprior)
-thetaBootShrink=(bootdat*n+prior*nprior)/(n+nprior)
-#cat(theta0Shrink,quantile(thetaBootShrink),'\n')
-pivots=(thetaHat-bootdat)/sqrt(thetaBootShrink*(1-thetaBootShrink)/n)
-
-endpoints=thetaHat+sqrt(theta0Shrink*(1-theta0Shrink)/n)*quantile(pivots,prob=probs,type=6)
-if(!full) return(endpoints)
-return(list(endpoints=endpoints,pivots=pivots))
-}
-
 #### Some standard CI calculation functions
 
 wilsonCI<-function(phat,n,conf=0.9)
@@ -61,3 +27,12 @@ chigh=ifelse(phat==1,1,qbeta(1-(1-conf)/2,x+w1,n-x+w2))
 
 return(cbind(clow,chigh))
 }
+
+#' Linearly interpolate/extrapolate from a single segment.
+
+extrapol<-function(point1,point2,xout)
+{
+slope=(point2[2]-point1[2])/(point2[1]-point1[1])
+return(point2[2]+(xout-point2[1])*slope)
+}
+
