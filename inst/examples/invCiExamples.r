09833d86b61696685c1814423914e1191f340d9a
Assaf Oron <assaf.oron@gmail.com>
2021-08-22 20:32:48 +0300

    Trying to build for CRAN...

--- inst/examples/invCiExamples.r
+++ inst/examples/invCiExamples.r
@@ -3,6 +3,9 @@
 dat=doseResponse(y=c(1/7,1/8,1/2,1/4,4/17),wt=c(7,24,20,12,17))
 # The experiment's goal is to find the 30th percentile
 quick1=quickIsotone(dat)
+# For inverse confidence intervals "the long way", 
+#    we need a full CIR output object:
+fwd1=cirPAVA(dat, full=TRUE)
 invDelta=deltaInverse(dat)
 
 ### Showing the data and the estimates
