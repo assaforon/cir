5df8ad2f475fd831c014b819f42a1d6345106f4c
Assaf Oron <assaf.oron@gmail.com>
2023-02-19 19:30:08 -0800

    Ok, finally understood the vignette force thing (maybe)

--- inst/examples/classExamples.r
+++ inst/examples/classExamples.r
@@ -1,8 +1,12 @@
 ## Summary of raw data from the notorious Neuenschwander et al. (Stat. Med., 2008) trial
-neundatTrace=DRtrace(x=c(rep(1:4,each=4),7,7,rep(6,9)),y=c(rep(0,16),1,1,rep(c(0,0,1),2),0,0,0))
+## Note the use of the 'cohort' argument to specify the cohort order
+neundatTrace=DRtrace(x=c(rep(1:4,each=4), 7, 7, rep(6,9)),
+                     y=c(rep(0,16), 1,1, rep(c(0,0,1),2), 0,0,0), 
+                     cohort = rep(1:8, c(4,4,4,4, 2, 3,3,3)))
 par(mar=c(3,3,3,1),mgp=c(2,.5,0),tcl=-0.25)
 layout(t(1:2))
-plot(neundatTrace,main="N. et al. (2008) Cohort Trace",ylab="Ordinal Dose Level",cex.main=1.5)
+plot(neundatTrace,main="N. et al. (2008) Cohort Trace", xlab = 'Cohort', 
+          ylab="Ordinal Dose Level",cex.main=1.5)
 
 ## Same data, in 'doseResponse' format with actual doses rather than dose levels
 neundatDose=doseResponse(x=c(1,2.5,5,10,20,25),y=c(rep(0,4),2/9,1),wt=c(3,4,5,4,9,2))
