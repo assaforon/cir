37e0afa8a9e72fa9a14c26b80489658a8f827cab
Assaf Oron <assaf.oron@gmail.com>
2025-10-25 18:23:18 -0700

    fine tuning plotting utilities

--- R/methods.r
+++ R/methods.r
@@ -6,8 +6,8 @@
 #'
 #' Generic methods for dose-response trajectory/trace (\code{\link{DRtrace}}), and dose-response summary  (\code{\link{doseResponse}}) class objects. 
 
-#' The \code{\link{DRtrace}} plotting uses the typical convention of plotting dose-finding experimental trace, with dose levels (x) in the vertical axis and 1/0 responses (y) denoted via filled/empty circles, respectively. In other words, this generic plotting method is only relevant for binary 0/1 outcomes. If cohort information is provided via `x$cohort` (i.e., multiple observations considered as collected together rather than each data point sequentially), then the plotting will respect cohort structure.
-
+#' The \code{\link{DRtrace}} plotting uses the typical convention of plotting dose-finding experimental trace, with dose levels (x) in the vertical axis and 1/0 responses (y) denoted via filled/empty symbols, respectively. In other words, this generic plotting method is only relevant for binary 0/1 outcomes. If cohort information is provided via `x$cohort` (i.e., multiple observations considered as collected together rather than each data point sequentially), then the plotting will respect cohort structure.
+#'
 #' The \code{\link{doseResponse}} plotting has response rate on the y-axis and dose on the x-axis, and plots symbols whose area is proportional to the weights. 
 
 #' @seealso \code{\link{doseResponse}}, \code{\link{DRtrace}}
@@ -19,11 +19,10 @@
 #' @param connect logical: whether to connect the symbols (generic plotting type 'b'). Default \code{TRUE} for \code{\link{DRtrace}} and \code{FALSE} for \code{\link{doseResponse}}
 #' @param varsize 	(\code{doseResponse} only) logical, should symbol size vary by sample size? Default \code{TRUE}
 #' @param refsize 	(\code{doseResponse} only) a reference size by which the plotting sizes will be multiplied. Default is \code{1/sqrt(mean(dr$weight))}, scaled so that if `varsize = TRUE` the weighted-average symbol size is 1. If `varsize = FALSE`, this argument is equivalent to `cex` in an ordinary x-y `plot()` call.
-#' @param dosevals Dose values to be plotted along the x-axis (`plot.doseResponse`) or y-axis (`plot.DRtrace`) . If \code{NULL} (default), those will be the doses in the dataset (i.e.,\code{sort(unique(x$x))}).
+#' @param dosevals Dose values to be plotted along the x-axis (`plot.doseResponse`) or y-axis (`plot.DRtrace`) . Must be on the same scale as `x$x`. If \code{NULL} (default), those will be the doses in the dataset (i.e.,\code{sort(unique(x$x))}). 
 #' @param offset (\code{DRtrace} only) In case of a cohort-based experiment, the relative vertical offset between symbols for outcomes within the same cohort (as fraction of dose spacing). Default 0.2.
 #' @param ...	Other arguments passed on to \code{\link{plot}}. 
 #' 
-#' Conversely, putting values on a different scale into `dosevals`, or even text labels instead of numbers, won't work. For the former, change the scale at the source data (i.e., in the plotted object). For the latter, sorry but no solution at present. 
 
 #' @author Assaf P. Oron \code{<assaf.oron.at.gmail.com>}	  
 #' @example inst/examples/classExamples.r
@@ -52,7 +51,7 @@ if(length(unique(x$cohort)) < n) # Case with cohorts
   x$midpoint = rep(tapply(x$within, x$cohort, max), tapply(x$within, x$cohort, max)) / 2 + 0.5
   plot(x$cohort, x$x + spacing*offset*(x$within - x$midpoint), 
        pch = ifelse(x$y==1, ch1, ch1-15),
-       xaxt='n', yaxt='n', xlab=xlab, ylab=ylab, col=mcol, ...)
+       xaxt='n', yaxt='n', xlab=xlab, ylab=ylab, col=mcol, ylim = range(dosevals), ...)
   if(connect) points(unique(x$cohort), tapply(x$x, x$cohort, function(x) x[1]), 
                      type = 'b', cex=0, ...)
   axis(1, at = 1:max(x$cohort), ...)
@@ -73,9 +72,10 @@ axis(2, at=dosevals, ...)
 #' @rdname plot.DRtrace
 #' @export
 plot.doseResponse<-function(x, xlab="Dose", ylab="Response", pch='X', varsize=TRUE,
-                            refsize=sqrt(1/mean(x$weight)), connect=FALSE, mcol=1, 
+                            refsize = NULL, connect=FALSE, mcol=1, 
                             dosevals=NULL, ...) 
 {
+if(is.null(refsize)) refsize = sqrt(1/mean(x$weight) )
 cexy = refsize
 if(varsize) cexy = refsize * sqrt(x$weight)
 
@@ -84,7 +84,7 @@ if(is.null(dosevals[1]))  dosevals = sort(unique(x$x))
 #if(is.null(xlim)) xlim = range(x$x)
 
 plot(y~x, data=x, pch=pch, xlab=xlab, ylab=ylab, cex=cexy, xaxt="n",
-	type=ifelse(connect, 'b', 'p'), col=mcol, ...)
+	type=ifelse(connect, 'b', 'p'), col=mcol, xlim = range(dosevals), ...)
 
 axis(1, at=dosevals, ...)
 }
